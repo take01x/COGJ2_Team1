783a52a168aeabdb73850bd885ef75b021361395
negimochi <negimochi.tabetai@gmail.com>
2016-05-29 16:28:58 +0900

    SortingLayerSettingに名前変更

---
+++ unity_client/Assets/Scripts/SortingLayerSetting.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Renderer))]
+public class SortingLayerSetting : MonoBehaviour {
+
+    [SerializeField]
+    private string layerName = "UI";
+
+//    [SerializeField]
+//    private int orderInLayer = 0;
+
+
+    void OnValidate()
+    {
+        SetupLayerName();
+    }
+
+    void Awake()
+    {
+        SetupLayerName();
+    }
+
+
+    void SetupLayerName()
+    {
+        var renderer = GetComponent<Renderer>();
+        if (renderer != null)
+        {
+            renderer.sortingLayerName = layerName;
+        }
+
+    }
+}
